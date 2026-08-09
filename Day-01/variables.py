ee36df7eb59dffa807dc6ae83aa13da29a8e37d7
Yash Prajapati <yashprajapati09941@gmail.com>
2026-08-09 08:44:40 +0530

    Day 01: Practice variables

---
+++ Day-01/variables.py
@@ -0,0 +1,13 @@
+# Day 01 - Variables Practice
+
+name = "Yash"
+age = 20
+city = "Ahmedabad"
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Height:", height)
+print("Student:", is_student)
