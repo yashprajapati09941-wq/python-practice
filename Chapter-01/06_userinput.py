79e02da322b0ae2e98671d6917b1d71d4146ee6c
Yash Prajapati <yashprajapati09941@gmail.com>
2026-08-11 22:02:22 +0530

    Chapter 01: Add user input and calculator

---
+++ Chapter-01/06_userinput.py
@@ -0,0 +1,16 @@
+# User Input in Python
+
+# Taking user's name
+name = input("Enter your name: ")
+
+# Taking user's age
+age = input("Enter your age: ")
+
+# Taking user's city
+city = input("Enter your city: ")
+
+# Displaying the information
+print("\n--- User Information ---")
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
---
+++ Chapter-01/07_calculator.py
@@ -0,0 +1,12 @@
+#Calculation of Integers
+
+a=int(input("enter the number"))   #Take input from user
+b=int(input("enter the number"))
+
+#Perform operations
+print("multiplication:", a * b)
+print("submition:",a + b)
+print("division",a / b)
+print("subtraction",a - b)
+
+
