72d7cefa47983501437d147daa6753cc22634a9f
Yash Prajapati <yashprajapati09941@gmail.com>
2026-08-29 21:14:16 +0530

    Add enumerate function.

---
+++ Python-Learning/23_enumerate-func.py
@@ -0,0 +1,6 @@
+# Printing subjects with their numbers
+
+subjects = ["Python", "SQL", "HTML", "CSS", "JavaScript"]
+
+for number, subject in enumerate (subjects, start=0):
+    print(number, "-", subject)
